6ab7da29542222dd0aef425e6331f25b380854f5
phoenix <306761352@qq.com>
2012-10-26 11:11:43 +0800

    test

--- fw-johnsquibb/index.php
+++ fw-johnsquibb/index.php
@@ -1,10 +1,11 @@
 <?php
+// 本框架教程页面： http://johnsquibb.com/tutorials/mvc-framework-in-1-hour-part-one
 
 /**
  * Define document paths
  */
-define('SERVER_ROOT' , 'WEB_ROOT_FOLDER');
-define('SITE_ROOT' , 'http://yourdomain.com');
+define('SERVER_ROOT' , '/home/phoenix/public_html/gitprojects/phpframework');
+define('SITE_ROOT' , 'http://173.230.150.168/gitprojects/phpframework/fw-johnsquibb/');
 
 /**
  * Fetch the router
