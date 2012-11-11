19f35ec92a84a1ed035f87428763feeae07af880
phoenixg-compaq321 <306761352@qq.com>
2012-11-11 22:20:46 +0800

    add twitto

---
+++ twitto/c.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * /twitto.php?c=hello_world
+ * or 
+ * /twitto/hello_world
+ */
+
+function hello_world()
+{
+  echo 'Hello World!';
+}
+
+function a()
+{
+	echo 'This is aaa';
+}
+
+function b()
+{
+	echo 'This is bbb';
+}
