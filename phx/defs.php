0eaae8ee07f073ee08cd488d7df5c582af5ce5b3
phoenix <306761352@qq.com>
2012-11-16 17:45:20 +0800

    for phpcs test

---
+++ phx/defs.php
@@ -0,0 +1,14 @@
+<?php
+
+// define paths of folders
+define('DS', DIRECTORY_SEPARATOR);
+define('PATH_BASE', __DIR__ . DS);
+define('PATH_APP', PATH_BASE . 'app' . DS); 
+define('PATH_ASSETS', PATH_BASE . 'assets' . DS); 
+define('PATH_CORE', PATH_BASE . 'core' . DS); 
+define('PATH_CORE_LIBS', PATH_BASE . 'core' . DS . 'libs' . DS);
+define('PATH_CORE_HELPERS', PATH_BASE . 'core' . DS . 'helpers' . DS);
+
+// define paths of files
+define('EXT', '.php');
+define('FILE_BASE', PATH_BASE . 'index' . EXT);
--- phx/index.php
+++ phx/index.php
@@ -5,18 +5,7 @@ date_default_timezone_set('Asia/Shanghai');
 // include dBug class
 include('./core/debug/dBug.php');
 
-// define paths of folders
-define('DS', DIRECTORY_SEPARATOR);
-define('PATH_BASE', __DIR__ . DS);
-define('PATH_APP', PATH_BASE . 'app' . DS); 
-define('PATH_ASSETS', PATH_BASE . 'assets' . DS); 
-define('PATH_CORE', PATH_BASE . 'core' . DS); 
-define('PATH_CORE_LIBS', PATH_BASE . 'core' . DS . 'libs' . DS);
-define('PATH_CORE_HELPERS', PATH_BASE . 'core' . DS . 'helpers' . DS);
-
-// define paths of files
-define('EXT', '.php');
-define('FILE_BASE', PATH_BASE . 'index' . EXT);
+require('defs.php');
 
 // set error reporting level
 error_reporting(E_ALL);
