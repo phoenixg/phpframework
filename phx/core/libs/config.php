ca5cb93495c7f751c8ee556c6728a865c317cfcd
phoenixg-compaq321 <306761352@qq.com>
2012-11-17 21:13:01 +0800

    PSR-2 core/libs/config.php

--- phx/core/libs/config.php
+++ phx/core/libs/config.php
@@ -1,46 +1,40 @@
-<?php defined('DS') or die('Direct access forbid!');
+<?php //defined('DS') or die('Direct access forbid!');
 
-class Config {
+class Config{
 
-	public static $items = array();
+    public static $items = array();
 
-	public function __construct(array $arr)
-	{
-		static::$items = $arr;
-		//var_export(static::$items);
-		//var_dump(static::$items["application"]["aaa"]["ddd"]["eee"]["out"]);
-	}
+    public function __construct(array $arr)
+    {
+        static::$items = $arr;
+        //var_export(static::$items);
+        //var_dump(static::$items["application"]["aaa"]["ddd"]["eee"]["out"]);
+    }
 
-	public static function get($str, $default = null)
-	{
-		$str = static::parse($str);
-		if (is_null($str) || !isset($str)) {
-			return $default;
-		}
+    public static function get($str, $default = null)
+    {
+        $str = static::parse($str);
+        if (is_null($str) || !isset($str)) {
+            return $default;
+        }
 
-		return $str;
-	}
+        return $str;
+    }
 
-	protected static function parse($str)
-	{
-		$str = 'static::$items["'.str_replace('.','"]["',$str).'"]';
-		$str_parent = substr($str, 0 , strrpos($str, '['));
+    protected static function parse($str)
+    {
+        $str = 'static::$items["'.str_replace('.','"]["',$str).'"]';
+        $str_parent = substr($str, 0 , strrpos($str, '['));
 
-		if(eval('return is_array('.$str_parent.');') && eval('return isset('.$str.');')){
-			return eval('return '.$str.';');
-		}
-		
-		return null;
-	}
+        if(eval('return is_array('.$str_parent.');') && eval('return isset('.$str.');')){
+            return eval('return '.$str.';');
+        }
+        
+        return null;
+    }
 
-	public static function has($str)
-	{
-		return ! is_null(static::get($str));
-	}
-
-
-
-
-
-
+    public static function has($str)
+    {
+        return ! is_null(static::get($str));
+    }
 }
