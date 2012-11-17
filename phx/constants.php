a44ecab66b61cf4e6d78c38ff43779f40ef50773
phoenixg-compaq321 <306761352@qq.com>
2012-11-17 21:08:44 +0800

    PSR-2 constant.php

--- phx/constants.php
+++ phx/constants.php
@@ -3,8 +3,8 @@
 // define paths of folders
 define('DS', DIRECTORY_SEPARATOR);
 define('PATH_BASE', __DIR__ . DS);
-define('PATH_APP', PATH_BASE . 'app' . DS); 
-define('PATH_ASSETS', PATH_BASE . 'assets' . DS); 
+define('PATH_APP', PATH_BASE . 'app' . DS);
+define('PATH_ASSETS', PATH_BASE . 'assets' . DS);
 define('PATH_CORE', PATH_BASE . 'core' . DS); 
 define('PATH_CORE_LIBS', PATH_BASE . 'core' . DS . 'libs' . DS);
 define('PATH_CORE_HELPERS', PATH_BASE . 'core' . DS . 'helpers' . DS);
