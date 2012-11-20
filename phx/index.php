ecbe1d3120d13abbc8bb1c7e702ca3112eaa0de7
phoenix <306761352@qq.com>
2012-11-20 14:01:05 +0800

    change error handler to anonymous function

--- phx/index.php
+++ phx/index.php
@@ -11,15 +11,17 @@ require('constants.php');
 error_reporting(E_ALL);
 
 // set error handler
-// use trigger_error("发生了一个错误") to trigger an error manually
-require(PATH_CORE_HANDLERS . 'error.php');
-set_error_handler('_PhxErrorHandler');
+// 全部预定义的Error常量对应的数值（即$errorNo）见：http://php.net/manual/en/errorfunc.constants.php
+// error发生时自动触发，或手动触发：trigger_error("发生了一个错误")
+set_error_handler(function ($errorNo, $errMsg, $errFilePath, $errLine){ 
+    echo '错误：' . $errorNo . '<br />';
+    echo '信息：' . $errMsg . '<br />';
+    echo '行号：' . $errLine . '<br />';
+    echo '位置：' . $errFilePath . '<br />';
 
-
-
-
-
-
+    $logInfo = '[' . date('Y/m/d H:i:s') . ']' . ' ' . $errorNo . ' ' . $errMsg . ' On Line:' . $errLine . ' ' . $errFilePath . "\n";
+    error_log($logInfo, 3, FILE_LOG_ERRORS);
+});
 
 
 // set exception handler
