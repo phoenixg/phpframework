733c96ffb469250c96c9abde1bc61af6e74af35b
phoenix <306761352@qq.com>
2012-11-26 14:45:10 +0800

    add mvc sample files

--- phx/app/mvc/controllers/default.php
+++ phx/app/mvc/controllers/default.php
@@ -1,7 +1,16 @@
 <?php
 class Default_Controller extends Controller {
-	public function index()
+	public function action_index()
 	{
 		echo 'you are in default controller and default method';
 	}
+
+	public function action_hello()
+	{
+		echo 'you are in default controller and hello method';
+		$this->_display('default.php');
+	}
+
+
+
 }
---
+++ phx/app/mvc/models/user.php
@@ -0,0 +1,7 @@
+<?php
+class User_Model extends Model {
+	public function query()
+	{
+		return array('apple', 'orange');
+	}
+}
